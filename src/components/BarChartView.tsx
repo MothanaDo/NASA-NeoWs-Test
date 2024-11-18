2bb3cb556ac7ee6bb899b388043a40f6cdfe0a77
MothannaDoubaa <en.mothanna@gmail.com>
2024-11-18 03:40:17 +0300

    Display a non-Stacked Bar Chart

---
+++ src/components/BarChartView.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import { BarChartProps } from "../types/types";
+
+export const BarChartView: React.FC<BarChartProps> = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={900}>
+      <BarChart
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar
+          dataKey="estimated_diameter.kilometers.estimated_diameter_min"
+          fill="#8884d8"
+        />
+        <Bar
+          dataKey="estimated_diameter.kilometers.estimated_diameter_max"
+          fill="#82ca9d"
+        />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
